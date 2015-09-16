704bb68d82df07659949bc6af5546779ba38efe2
Rafael Espinoza <rafael@rafaelespinoza.com>
2015-09-15 18:06:55 -0700

    Add a parseRad function & update ref to new 'bin/'
    This function was added directly to the exports file. But I should
    consider moving the bulk of this into the 'src/' dir.
    
    Also, update the path to new bin/ directory for cli.js

--- index.js
+++ index.js
@@ -13,4 +13,55 @@ module.exports = {
   octToDec: convert.octToDec,
   octToBin: convert.octToBin,
   octToHex: convert.octToHex,
+  parseRad: parseRad
 };
+
+/**
+ * A buffed-up version of the standard `parseInt`
+ * @param str
+ * @param from
+ * @param to
+ * @return
+ */
+function parseRad(str, from, to) {
+  var fn = assignFn(from, to);
+  return convert[fn](str);
+}
+
+// Assign a method, depending on which two bases are passed in
+function assignFn(from, to) {
+  from = setDefault(10, from);
+  to   = setDefault(10, to);
+
+  from = numToWord(from);
+  to   = capitalize(numToWord(to));
+
+  return from + 'To' + to;
+
+  // return a copy of a string with its first letter capitalized
+  function capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+
+  // translate a number to its correlating module-specific namespace
+  function numToWord(num) {
+    num += '';
+    switch(num) {
+      case '2':
+        return 'bin';
+      case '8':
+        return 'oct';
+      case '10':
+        return 'dec';
+      case '16':
+        return 'hex';
+      default:
+        throw new Error('Unfortunately, base "' + num + '" is not supported');
+    }
+  }
+
+  // if `arg` is not provided, return a default value
+  function setDefault(defaultVal, arg) {
+    return (typeof arg === 'undefined' ? defaultVal : arg);
+  }
+}
